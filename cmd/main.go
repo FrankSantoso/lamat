0eacf6f1ef702b94906ea78b736060f049f4c626
Alex Frank Adhyatma <alex@solusi.tech>
2020-03-31 12:19:49 +0000

    gitignore: added vendor

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,75 @@
 package main
 
 import (
-	"flag"
+	"context"
+	"errors"
+	"github.com/FrankSantoso/lamat.git/internal/cfg"
+	"github.com/spf13/cobra"
 	"log"
+	"os"
+	"time"
+)
+
+const (
+	globalTimeout = 10 * time.Second
 )
 
 var (
-	cfgFile = flag.String("c", "config",
-		"config file containing spec in cfg package")
+	cfgFile string
+	rootCmd = &cobra.Command{
+		Use:   "lamat [-c] <config_file> [r|f] [args]",
+		Short: "Small utility to find geocode / reverse geocode",
+	}
+	findCmd = &cobra.Command{
+		Use:   "f [longitude] [latitude]",
+		Short: "Try finding address of inputted coordinates / geocode",
+		Long:  `Find address of inputted coordinates / geocode along with some extra details`,
+		Args:  cobra.ExactArgs(2),
+		Run:   findGeocode,
+	}
+	reverseCmd = &cobra.Command{
+		Use:   "r [address]",
+		Short: "Try finding geocode information from specified input address",
+		Long:  `Find geocode information from inputted address`,
+		Args:  cobra.ExactArgs(1),
+		Run:   findRevGeocode,
+	}
 )
 
 func main() {
-	flag.Parse()
-	if cfgFile == nil {
-		log.Fatalf("configuration file must be set")
+	rootCmd.PersistentFlags().StringVarP(&cfgFile, "cfg", "", `
+	`, "config files containing nominatim & google api keys")
+	rootCmd.AddCommand(findCmd, reverseCmd)
+	// create new context
+	ctx, _ := context.WithTimeout(context.Background(), globalTimeout)
+	config, err := cfg.ReadConfig(cfgFile)
+	if err != nil {
+		log.Fatalf("Config file error: \n%v", err)
+		os.Exit(1)
 	}
+	// embeds config to the context
+	newCtx := context.WithValue(ctx, "cfg", config)
+	rootCmd.ExecuteContext(newCtx)
+}
 
+func getConfigFromContext(ctx context.Context) (*cfg.Config, error) {
+	c := ctx.Value("cfg").(cfg.Config)
+	if &c == nil {
+		return nil, errors.New("No config in context")
+	}
+	return &c, nil
+}
+
+func findGeocode(c *cobra.Command, args []string) {
+	config, err := getConfigFromContext(c.Context())
+	if err != nil {
+		log.Fatalf("Config file not found in context: %v", err)
+	}
+}
+
+func findRevGeocode(c *cobra.Command, args []string) {
+	config, err := getConfigFromContext(c.Context())
+	if err != nil {
+		log.Fatalf("Config file not found in context: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/FrankSantoso/lamat.git
 
 go 1.14
 
-require github.com/spf13/viper v1.6.2
+require (
+	github.com/FrankSantoso/geo-golang v1.6.2-0.20200330144737-a7821a872a1d
+	github.com/fatih/color v1.9.0
+	github.com/klauspost/compress v1.10.3 // indirect
+	github.com/prometheus/common v0.4.0
+	github.com/spf13/cobra v0.0.7
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.6.2
+)
